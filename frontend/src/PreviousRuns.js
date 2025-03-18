e4240bd9fb508c5ce62f2d275d792aeb63044254
Nishant Nagururu <nishantnvn05@gmail.com>
2025-03-18 18:42:09 -0400

    stuff

---
+++ frontend/src/PreviousRuns.js
@@ -0,0 +1,18 @@
+// PreviousRuns.js
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+
+const PreviousRuns = () => {
+  return (
+    <Box sx={{ pt: 4, pb: 6 }}>
+      <Typography variant="h4" component="h1" gutterBottom align="center">
+        Previous Runs
+      </Typography>
+      <Typography variant="body1" align="center">
+        This page is under construction.
+      </Typography>
+    </Box>
+  );
+};
+
+export default PreviousRuns;
